8e52fb919aa5ee446a062cc6fe37c08a20197858
derrick b <dbozich1@gmail.com>
2018-09-03 18:41:34 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,9 +11,9 @@ class PostsController < ApplicationController
       @posts = Post.by_author(params[:author])
     elsif !params[:date].blank?
       if params[:date] == "Today"
-        @posts = Post.old_news
-      else
         @posts = Post.from_today
+      else
+        @posts = Post.old_news
       end
     else
       # if no filters are applied, show all posts
